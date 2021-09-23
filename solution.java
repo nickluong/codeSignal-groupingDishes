8531999c3cca738979f2bb5d5391cdab368d495f
nickluong <39754434+nickluong@users.noreply.github.com>
2021-09-23 16:29:47 -0400

    Update solution.java

--- solution.java
+++ solution.java
@@ -1,3 +1,10 @@
+/*
+You are given a list dishes, where each element consists of a list of strings beginning with the name of the dish, followed by all the ingredients used in preparing it. 
+You want to group the dishes by ingredients, so that for each ingredient you'll be able to find all the dishes that contain it (if there are at least 2 such dishes).
+Return an array where each element is a list beginning with the ingredient name, followed by the names of all the dishes that contain this ingredient. 
+The dishes inside each list should be sorted lexicographically, and the result array should be sorted lexicographically by the names of the ingredients.
+*/
+
 String[][] groupingDishes(String[][] dishes) {
     HashMap<String, ArrayList<String>> map = new HashMap<>();
     for(String[] ingred: dishes){
